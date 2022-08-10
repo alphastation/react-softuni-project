66965a773604fafadddb93d2433d01253297f08b
alphastation <alexander111111@gmail.com>
2022-08-10 09:53:22 +0100

    react-router-dom added to the project; folder named pages created containing 6 jsx files for the main app views; all of them are imported into App.js file and included in the Routes structure.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Dashboard from './pages/Dashboard.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import Offers from './pages/Offers.jsx';
+import Profile from './pages/Profile.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+
 
 function App() {
   return (
     <>
-      <h1>Hellow from my App!!!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+
+      {/* <h1>Hellow from my App!!!</h1> */}
     </>
   );
 }
